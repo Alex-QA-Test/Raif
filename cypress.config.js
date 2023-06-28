8d953904c3fb9e7fdad1cc7562663ab0531b3116
DESKTOP-EJMMH2P\Alex <castlee1555@gmail.com>
2023-06-28 22:08:06 +0300

    Добавил плагин Cypress split

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+const cypressSplit = require('cypress-split')
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       allureWriter(on, config);
+      cypressSplit(on, config)
             return config;
       // implement node event listeners here
     },
